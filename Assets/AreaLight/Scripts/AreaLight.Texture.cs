1ca353c8409aadc5d2eb997b8ecedc5b40ed5f95
Nicole <nicolechen235@gmail.com>
2016-11-28 15:38:26 -0800

    Add texture manager

--- Assets/AreaLight/Scripts/AreaLight.Texture.cs
+++ Assets/AreaLight/Scripts/AreaLight.Texture.cs
@@ -8,53 +8,19 @@ using System.Text;
 public partial class AreaLight : MonoBehaviour
 {
     [Header("Texture")]
+    public bool m_UseTextureLight;
     public Texture2D m_LightTexture;
-    private Texture2D m_FilterLightTexture;
+    public Texture2D m_FilterLightTexture;
     public string m_TextureFile;
-    private bool initTexture = false;
-    private int m_texHeight;
-    private int m_texWidth;
 
     void InitLightTexture()
     {
-
-        //#TODO save this texture in a struct
-        if (initTexture) {
-            Debug.Log("Init Again");
+        if (!m_UseTextureLight) {
+            Debug.LogWarning("Did not use the texture light for " + this.name);
+            m_FilterLightTexture = null;
             return;
         }
 
-        initTexture = true;
-        BinaryReader reader = new BinaryReader(File.Open(m_TextureFile, FileMode.Open, FileAccess.Read));
-
-        reader.BaseStream.Seek(12, SeekOrigin.Begin);
-        m_texHeight = reader.ReadInt32();
-
-        reader.BaseStream.Seek(16, SeekOrigin.Begin);
-        m_texWidth = reader.ReadInt32();
-
-        reader.BaseStream.Seek(28, SeekOrigin.Begin);
-        int mipmapCount = reader.ReadInt32();
-
-        int offset = 128;
-        int size = 1 << (mipmapCount - 1);
-
-        m_FilterLightTexture = new Texture2D(m_texWidth, m_texHeight, TextureFormat.RGBA32, true);
-        m_FilterLightTexture.wrapMode = TextureWrapMode.Clamp;
-        for (int mIndex = 0; mIndex < mipmapCount; mIndex++) {
-            reader.BaseStream.Seek(offset, SeekOrigin.Begin);
-            byte[] tmpBytes = reader.ReadBytes(size * size * 4);
-            Texture2D tempTex = new Texture2D(size, size, TextureFormat.RGBA32, false);
-            tempTex.LoadRawTextureData(tmpBytes);
-            tempTex.Apply();
-
-            Graphics.CopyTexture(tempTex, 0, 0, m_FilterLightTexture, 0, mIndex);
-
-            offset += size * size * 4;
-            size /= 2;
-        }
-        
-        m_FilterLightTexture.Apply(false);
-
+        m_FilterLightTexture = AreaLightTextureManager.Instance.AddTexture(m_TextureFile);
     }
 }
---
+++ Assets/AreaLight/Scripts/AreaLightTextureManager.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaLightTextureManager
+{
+
+    private static AreaLightTextureManager instance;
+
+    private Dictionary<string, Texture2D> textures;
+    private AreaLightTextureManager() {
+        textures = new Dictionary<string, Texture2D>();
+    }
+
+    public static AreaLightTextureManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new AreaLightTextureManager();
+            }
+            return instance;
+        }
+    }
+
+    public Texture2D AddTexture(string texName) {
+        if (textures.ContainsKey(texName)) {
+            return textures[texName];
+        }
+        Texture2D filteredLightTexture;
+        BinaryReader reader = new BinaryReader(File.Open(texName, FileMode.Open, FileAccess.Read));
+
+        reader.BaseStream.Seek(12, SeekOrigin.Begin);
+        int texHeight = reader.ReadInt32();
+
+        reader.BaseStream.Seek(16, SeekOrigin.Begin);
+        int texWidth = reader.ReadInt32();
+
+        reader.BaseStream.Seek(28, SeekOrigin.Begin);
+        int mipmapCount = reader.ReadInt32();
+
+        int offset = 128;
+        int size = 1 << (mipmapCount - 1);
+
+        filteredLightTexture = new Texture2D(texWidth, texHeight, TextureFormat.RGBA32, true);
+        filteredLightTexture.wrapMode = TextureWrapMode.Clamp;
+        for (int mIndex = 0; mIndex < mipmapCount; mIndex++)
+        {
+            reader.BaseStream.Seek(offset, SeekOrigin.Begin);
+            byte[] tmpBytes = reader.ReadBytes(size * size * 4);
+            Texture2D tempTex = new Texture2D(size, size, TextureFormat.RGBA32, false);
+            tempTex.LoadRawTextureData(tmpBytes);
+            tempTex.Apply();
+
+            Graphics.CopyTexture(tempTex, 0, 0, filteredLightTexture, 0, mIndex);
+
+            offset += size * size * 4;
+            size /= 2;
+        }
+
+        filteredLightTexture.Apply(false);
+
+        textures.Add(texName, filteredLightTexture);
+        return filteredLightTexture; 
+    }
+}
